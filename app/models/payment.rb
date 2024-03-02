129c854081c29499133a1f750dc3410865f63d25
Dyego Penha <dyegopenha@hotmail.com>
2024-03-01 22:03:10 -0300

    payment validations

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,3 +1,6 @@
 class Payment < ApplicationRecord
   has_one_attached :file
+
+  validates :amount, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :kind, inclusion: { in: %w(salario conta outro) }
 end
