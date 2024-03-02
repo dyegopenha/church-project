c56f308f3fe8f26aaa3fd7cfb9ab00519dc02952
Dyego Penha <dyegopenha@hotmail.com>
2024-03-01 22:14:46 -0300

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   belongs_to :department
   has_one_attached :avatar
+
+  validates :name, presence: true
+  validates :cpf, presence: true, uniqueness: true
+  validates :email, presence: true
+  validates :phone, presence: true
+  validates :zipcode, presence: true
+  validates :address, presence: true
+  validates :address_number, presence: true
+  validates :birthday, presence: true
+  validates :office, inclusion: { in: %w(nenhum diacono presbitero pastor) }
 end
