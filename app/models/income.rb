4ad7bf0cf4e088e49d0153910396cde8748eadac
Dyego Penha <dyegopenha@hotmail.com>
2024-03-01 22:01:01 -0300

    income validations

--- app/models/income.rb
+++ app/models/income.rb
@@ -1,3 +1,6 @@
 class Income < ApplicationRecord
   has_one_attached :file
+
+  validates :amount, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :kind, inclusion: { in: %w(dizimo oferta) }
 end
