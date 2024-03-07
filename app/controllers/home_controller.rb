69604352dd9236a5de27aabbf225266f00fb7722
Dyego Penha <dyegopenha@hotmail.com>
2024-03-07 16:19:17 -0300

    add range limit to chart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @incomes = Income.all.sum(:amount)
-    @payments = Payment.all.sum(:amount)
-    @balance = @incomes - @payments
+    @income_sum_by_month = Income.group_by_month(:when, range: 5.months.ago..Time.now).sum(:amount)
+    @income_sum_by_kind = Income.group(:kind).sum(:amount)
+
+    @payment_sum_by_month = Payment.group_by_month(:when, range: 5.months.ago..Time.now, format: "%b %Y").sum(:amount)
+    @payment_sum_by_kind = Payment.group(:kind).sum(:amount)
+
+    @balance = Income.all.sum(:amount) - Payment.all.sum(:amount)
   end
 end
