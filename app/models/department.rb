a894ba39b4da4c9f3a40f88b4337b52a49a928b9
Dyego Penha <dyegopenha@hotmail.com>
2024-03-14 10:27:18 -0300

    add events to header and some validations

--- app/models/department.rb
+++ app/models/department.rb
@@ -2,6 +2,7 @@ class Department < ApplicationRecord
   has_one_attached :avatar
   has_many :users
   has_many :documents
+  has_many :events
 
   validates :name, presence: true, uniqueness: true
 
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,29 @@
 class Event < ApplicationRecord
   belongs_to :department
+
+  validates :title, presence: true
+  validates :start_date, :end_date, presence: true
+  validate :end_date_after_start_date
+  validate :old_date
+
+  def unavailable_dates
+    Event.where('start_date > ?', Time.now).pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
+
+  private
+
+  def end_date_after_start_date
+    if end_date < start_date
+      errors.add(:end_date, "must be after the start date")
+    end
+  end
+
+  def old_date
+    current = Time.now
+    if start_date < current
+      errors.add(:start_date, "must be after #{current}")
+    end
+  end
 end
